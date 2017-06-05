6692318d56857edc4f4940a28bdc8257415648c7
Saúl <saul@blackfire.local>
2017-06-04 20:46:36 -0500

    Debug

--- wp-content/themes/lassic-theme/include/theme-components/cs-header/header_functions.php
+++ wp-content/themes/lassic-theme/include/theme-components/cs-header/header_functions.php
@@ -164,7 +164,10 @@ if (!class_exists('cs_mega_menu_walker')) {
 				}
 			}
 			$class_names = join( " $mega_menu ", apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
-			$class_names = ' class="'. esc_attr( sanitize_html_class($class_names) ) . ' current-menu-item"';
+			$class_names = ' class="'. esc_attr( sanitize_html_class($class_names) ) . '"'; /* current-menu-item */
+			
+			error_log('+++ item: '.var_export($item,TRUE) ,0)
+			
 			$output .= $indent . '<li id="menu-item-'. $item->ID . '"' . $value . $class_names .'>';
 			$attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
  			$attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
