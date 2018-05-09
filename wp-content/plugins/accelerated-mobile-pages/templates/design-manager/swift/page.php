bd65887af8785cb7db532a1d7b106975317db4fc
root <root@WEBPAGETARS.3wz5qiojgo4utbhnpqsswk255c.ax.internal.cloudapp.net>
2018-05-09 09:24:32 -0500

    accelerated mobile apps

---
+++ wp-content/plugins/accelerated-mobile-pages/templates/design-manager/swift/page.php
@@ -0,0 +1,23 @@
+<?php global $redux_builder_amp;
+amp_header(); ?>
+<div class="sp">
+	<div <?php if(!checkAMPforPageBuilderStatus(get_the_ID())){ ?>class="cntr"<?php } ?>>
+		<?php if(!checkAMPforPageBuilderStatus(get_the_ID())){ ?>
+			<?php if ( true == $redux_builder_amp['ampforwp-bread-crumb'] ) {
+				amp_breadcrumb();
+			}?>
+		 	<?php amp_title(); ?>
+		<?php } ?>
+       <div class="pg">
+			<div class="cntn-wrp">
+				<?php amp_content(); ?>
+			</div>
+			<?php if(!checkAMPforPageBuilderStatus(get_the_ID())){ ?>
+			<div class="cmts">
+				<?php amp_comments();?>
+			</div>
+			<?php } ?>
+		</div>
+	</div>
+</div>
+<?php amp_footer()?>
